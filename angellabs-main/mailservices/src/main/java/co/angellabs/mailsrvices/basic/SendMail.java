5b2f17cb9460e683abd4d763eb0b327ff626467d
sandeep <sandeep.mittakola@gmail.com>
2013-02-09 03:11:59 -0800

    commit

---
+++ angellabs-main/mailservices/src/main/java/co/angellabs/mailsrvices/basic/SendMail.java
@@ -0,0 +1,47 @@
+package co.angellabs.mailsrvices.basic;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+public class SendMail {
+	
+	public void confirmMail(String from,String to) {
+		Properties props = new Properties();
+	
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+ 
+		Session session = Session.getDefaultInstance(props,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("contact@viswgnabharathi.com","v.bharathi");
+				}
+			});
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to));
+			message.setSubject("Confirm mail");
+			message.setText("Dear User," +
+					"\n\n wel come to angellabs!");
+ 
+			Transport.send(message);
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
